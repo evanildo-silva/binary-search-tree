a4d7876ba095043842b6f7633f32e35ce8f46c86
Evanildo Silva Ferreira <evanildo.ferreira@livsaude.com.br>
2025-03-27 11:36:28 -0300

    feat: add hasrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { HashRouter } from "react-router-dom";
 import TreeVisualizer from "./TreeVisualizer";
 
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <HashRouter>
-        <TreeVisualizer />
-      </HashRouter>
+      <TreeVisualizer />
     </div>
   );
 }
